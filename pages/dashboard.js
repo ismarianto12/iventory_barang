490dff9f87b4b430d8692db7caa847ef6c286ea1
Isma Rianto <ismarianto97@gmail.com>
2022-07-29 00:40:21 +0700

    asdadas

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,13 +3,15 @@ import Head from "next/head"
 import Template from "../components/template/template"
 import Card from 'react-bootstrap/Card'
 import Chart from 'chart.js/auto'
-
+import * as Icon from 'react-feather'
 
 const Dashboard = () => {
 
     useEffect(() => {
 
         const ctx = document.getElementById('myChart').getContext('2d')
+        const crt = document.getElementById('Bar').getContext('2d')
+
         const myChart = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -45,51 +47,45 @@ const Dashboard = () => {
             }
         });
 
-        const DATA_COUNT = 7;
-        const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
-
-        const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-        const datax = {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'Dataset 1',
-                    data: 1,
-                    // borderColor: Utils.CHART_COLORS.red,
-                    backgroundColor: 'rgba(255, 99, 132, 1)', // Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-                    stack: 'combined',
-                    type: 'bar'
-                },
-                {
-                    label: 'Dataset 2',
-                    data: 2,
-                    // borderColor: Utils.CHART_COLORS.blue,
-                    backgroundColor:  'rgba(255, 99, 132, 1)', // Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-                    stack: 'combined'
-                }
-            ]
-        };
-
-        const klChart = new Chart(ctx, {
+        const jk = new Chart(crt, {
             type: 'line',
-            data: datax,
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
             options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Chart.js Stacked Line/Bar Chart'
-                    }
-                },
                 scales: {
                     y: {
-                        stacked: true
+                        beginAtZero: true
                     }
                 }
-            },
+            }
         });
+
+
         return () => {
             myChart.destroy()
-            klChart.destroy()
+            jk.destroy()
 
         }
     });
@@ -103,13 +99,20 @@ const Dashboard = () => {
             <>
                 <div className="row">
                     <div className="col-md-6">
+                        <h4><Icon.List /> Statistik Penjualan</h4>
                         <canvas id="myChart" width="auto" height="155"></canvas>
 
                     </div>
                     <div className="col-md-6">
-                        <canvas id="klChart" width="auto" height="155"></canvas>
+                        <h4><Icon.BarChart2 /> Barang Dan stok</h4>
+
+                        <canvas id="Bar" width="auto" height="140"></canvas>
 
                     </div>
+
+                    <h4><Icon.List /> Statistik Penjualan</h4>
+                    <br /> <br />
+                    <div className='clearfix'></div>
                     {[
                         'Primary',
                         'Secondary',
